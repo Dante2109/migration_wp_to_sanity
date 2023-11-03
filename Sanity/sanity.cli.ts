ff21274610c3a0ac94bdff5f6e97965b5b4e04e8
dante2109 <akshayverma4299@gmail.com>
2023-11-03 17:50:40 +0530

    sanity backend generated

--- Sanity/sanity.cli.ts
+++ Sanity/sanity.cli.ts
@@ -4,5 +4,13 @@ export default defineCliConfig({
   api: {
     projectId: 'ripwd1f6',
     dataset: 'production'
-  }
+  },
+  graphql: [
+    {
+      tag: "default",
+      playground: true,
+      generation: "gen3",
+      nonNullDocumentFields: false,
+    }
+  ]
 })
